2c8cef15f4b5323dcede3eba00635e35c82c5e87
enas <enas.elashrey@gmail.com>
2026-05-18 13:11:41 +0300

    adding overflow in out-box

--- handler/hendler.go
+++ handler/hendler.go
@@ -9,8 +9,8 @@ import (
 type PageData struct {
 	Result template.HTML
 	Text   string
-	 Font   string   // 👈 ضيفي دي
-    Color  string   // 👈 ودي
+	Font   string // 👈 ضيفي دي
+	Color  string // 👈 ودي
 }
 
 func renderTemplate(
@@ -73,8 +73,8 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html", PageData{
 		Result: template.HTML(result),
 		Text:   text,
-		  Font:   font,   // 👈 مهم
-    Color:  color,  // 👈 مهم
+		Font:   font,  // 👈 مهم
+		Color:  color, // 👈 مهم
 	})
 }
 
